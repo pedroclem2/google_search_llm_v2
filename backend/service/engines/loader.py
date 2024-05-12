3307dde157aecd9a916741ae07e64f5938954fc1
Pedro Clem <pedroclem2@gmail.com>
2024-05-12 21:23:52 +0100

    :sparkles: Main.py added

--- backend/service/engines/loader.py
+++ backend/service/engines/loader.py
@@ -1,16 +1,9 @@
-import asyncio
 import os
 from loguru import logger
-from ..engines.url_fetcher import *
-from ..configs.service_config import get_openai_api_key
-
+from .url_fetcher import *
 from langchain_community.document_loaders import AsyncHtmlLoader
 from langchain_community.document_transformers import Html2TextTransformer
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_community.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQA
-from langchain_community.document_loaders import DirectoryLoader
+
 
 logger.add("debug.log", rotation="1 week")
 
@@ -51,34 +44,4 @@ async def load_and_transform(search_query):
 
     return content_files if content_files else None
 
-async def main():
-    """
-    Main function to execute the load, transform, and query pipeline.
-    """
-    search_query = input("Enter your search query: ")
-    filenames = await load_and_transform(search_query)
 
-    if not filenames:
-        logger.error("No content available after multiple attempts.")
-        return
-
-    loader = DirectoryLoader('service/databases/raw_files', glob="*.txt")
-    docs = loader.load()
-    if not docs:
-        raise ValueError("No documents found in the directory.")
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(docs)
-
-    os.environ["OPENAI_API_KEY"] = get_openai_api_key()
-    embeddings = OpenAIEmbeddings(deployment="text-embedding-3-small", chunk_size=500)
-    docsearch = Chroma.from_documents(documents=texts, embedding=embeddings, persist_directory="service/databases/chromadb")
-
-    llm = ChatOpenAI()
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="map_reduce", retriever=docsearch.as_retriever(), return_source_documents=False)
-
-    response = qa.run(search_query)
-    logger.info(response)
-
-if __name__ == "__main__":
-    asyncio.run(main())
